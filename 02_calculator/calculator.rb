a3ef9a88ff4b2d0ee89697933d84aec35e97a363
Charlotte Hughes <hues.charlotte@gmail.com>
2020-01-05 11:52:46 +0000

    Added tests to calculator and solved them

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
-#write your code here
+def add(x, y)
+    x+y
+end
+
+def subtract(x, y)
+    x-y
+end
+
+def sum(arr)
+    result = 0
+    i = 0
+    while (i<arr.length)
+        result += arr[i]
+        i+=1
+    end
+    result
+end
+
+def multiply(arr)
+    result = 1
+    i = 0
+    while (i < arr.length)
+        result *= arr[i]
+        i+=1
+    end
+    result
+end 
+
+def power(x, y)
+    x**y
+end
+
+def factorial(x)
+    result = 1
+    
+        if x == 0 || x == 1
+    
+        else
+            i = 1
+            while i<=x
+                result *= i
+                i += 1
+            end
+        end
+    result 
+    end
